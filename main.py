9f81f0949be6fbf4aac4be2f2555c0c2d8b6f250
ansil <ansilproabl@gamil.com>
2023-06-06 19:38:49 +0530

    video capturing
    started video capturing and added comment

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # importing needed modules
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(0)  # creating video capture object
+cap.set(3, 640)  # setting width of frame
+cap.set(4, 480)  # setting height of frame
 
+# capturing video (images) for detecting object using a while loop
 while cap.isOpened():
     ret, img = cap.read()
     if ret:
-        cv.imshow("im", img)
-    if cv.waitKey(1) == ord('q'):
+        cv.imshow("window", img)
+    if cv.waitKey(1) == ord('q'):  # stop capturing when user interrupts
         break
 
-cap.release()
+cap.release()  # deallocates memory and clears
 cv.destroyAllWindows()
