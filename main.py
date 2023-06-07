9a2f78ab76383be1cc0d7ca5e3ef4a6405797e09
ansil <Ansilproabl@gmail.com>
2023-06-07 20:28:30 +0530

    trackbars for canny edge detector thresholds

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # importing needed modules
-import cv2 as cv
+
 from module.preprocessing import *
 
 cap = cv.VideoCapture(0)  # creating video capture object
 cap.set(3, 640)  # setting width of frame
 cap.set(4, 480)  # setting height of frame
 
+# creating a trackbar window
+cv.namedWindow("edge")
+cv.createTrackbar('start', 'edge', 50, 255, empty)
+cv.createTrackbar('end', 'edge', 100, 250, empty)
+
 # capturing video (images) for detecting object using a while loop
 while cap.isOpened():
     ret, img = cap.read()
     if ret:
-        cv.imshow('edited',preProcessing(img))  # preprocessed image
+        cv.imshow('edited', preProcessing(img, trackbar=True))  # preprocessed image
         cv.imshow("window", img)
     if cv.waitKey(1) == ord('q'):  # stop capturing when user interrupts
         break
 
 cap.release()  # deallocates memory and clears
-
--- module/preprocessing.py
+++ module/preprocessing.py
@@ -1,6 +1,25 @@
 import cv2 as cv
 
-def preProcessing(img):
-    img = cv.GaussianBlur(img,(5,5),10)
-    img = cv.Canny(img,50,250)
+
+def empty(a):
+    pass
+
+
+def preProcessing(img, trackbar=False):
+    """
+    returns pre-processed image which include Gaussian blurring, Canny edge detection, dilation steps
+
+    :param img: image to be processed
+    :param trackbar: trackbar position to set canny edge detection thresholds
+    :return: pre-processed image
+    """
+
+    img = cv.GaussianBlur(img, (5, 5), 10)
+    if trackbar:
+        start = cv.getTrackbarPos('start', 'edge')
+        end = cv.getTrackbarPos('end', 'edge')
+        img = cv.Canny(img, start, end)
+    else:
+        img = cv.Canny(img, 50, 250)
+    img = cv.dilate(img, (25, 25))
     return img
