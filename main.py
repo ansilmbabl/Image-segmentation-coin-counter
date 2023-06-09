ef2a7df9615e29612ac93428d2f10649312adb69
ansil <Ansilproabl@gmail.com>
2023-06-09 19:39:28 +0530

    bounding rectangle

--- main.py
+++ main.py
@@ -21,8 +21,17 @@ while cap.isOpened():
     if ret:
         img_pre = preProcessing(img, trackbar=True)
         cv.imshow('edited', img_pre)  # preprocessed image
-        cnt, hir = cv.findContours(img_pre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # cv.imshow("cnt", cnt)
+        # finding contours
+        cnt, hir = cv.findContours(img_pre, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        drawing = np.zeros((img_pre.shape[0], img_pre.shape[1], 3), dtype=np.uint8)
+        cv.drawContours(drawing,cnt,1,(255,255,0),3)
+        cv.imshow("draw", drawing)
+        # drawing bounding rectangle
+        for i in cnt:
+            x,y,w,h = cv.boundingRect(i)
+            cv.rectangle(img_pre,(x,y),(x+w,y+h),(255,0,0),4)
+
+        cv.imshow("cnt", img_pre)
         cv.imshow("window", img)
     if cv.waitKey(1) == ord('q'):  # stop capturing when user interrupts
         break
