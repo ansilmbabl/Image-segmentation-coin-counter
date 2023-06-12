d5366aeed63ccdf71401322dffe3774b39235e63
ansil <Ansilproabl@gmail.com>
2023-06-12 15:12:11 +0530

    Add conditon for checking coins (round shape) by len(contour)

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ cap = cv.VideoCapture(0)  # creating video capture object
 cap.set(3, 640)  # setting width of frame
 cap.set(4, 480)  # setting height of frame
 
-
 # creating a trackbar window
 cv.namedWindow("edge")
 cv.createTrackbar('start', 'edge', 50, 255, empty)
 cv.createTrackbar('end', 'edge', 100, 250, empty)
-
 
 # capturing video (images) for detecting object using a while loop
 while cap.isOpened():
@@ -22,13 +20,15 @@ while cap.isOpened():
         img_pre = preProcessing(img, trackbar=True)
         cv.imshow('edited', img_pre)  # preprocessed image
         # finding contours
-        cnt, hir = cv.findContours(img_pre, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        cv.drawContours(img,cnt,-1,(0,255,0),3)
-        
+        cnt, hir = cv.findContours(img_pre, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print(len(cnt))
+
         # drawing bounding rectangle
         for i in cnt:
-            x,y,w,h = cv.boundingRect(i)
-            cv.rectangle(img_pre,(x,y),(x+w,y+h),(255,0,0),4)
+            if 20 >= len(cnt) >= 8:
+                cv.drawContours(img, cnt, -1, (0, 255, 0), 3)
+                x, y, w, h = cv.boundingRect(i)
+                cv.rectangle(img_pre, (x, y), (x + w, y + h), (255, 0, 0), 4)
 
         cv.imshow("cnt", img_pre)
         cv.imshow("window", img)
--- readme.md
+++ readme.md
@@ -7,6 +7,6 @@
    2. Canny Edge detection to detect the edges
    3. Dilation to thicken the edges
    4. Morphological operation to close the discontinuity
-3. Finding Contours.
+3. [Finding Contours](https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html).
 4. Differentiating coins by size and color.
-5. Counting the values of the coin and displaying it.
+5. Counting the values of the coin and displaying [it](##steps).
