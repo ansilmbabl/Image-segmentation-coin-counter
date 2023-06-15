da9b7f0464b2a0cde2f2e28b623251a92f067fa3
ansil <Ansilproabl@gmail.com>
2023-06-15 06:51:48 +0530

    updated test.py
    added comments to the lines
    added a demo video

--- test.py
+++ test.py
@@ -1,88 +1,111 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture('http://192.168.1.72:8080/video')
+# creating video capture object (ip webcam)
+cap = cv.VideoCapture('http://192.168.1.71:8080/video')
+# resizing the output window
 cv.namedWindow('out', cv.WINDOW_NORMAL)
 cv.resizeWindow('out', 640, 360)
 
 
-def pas(a):
+def empty(a):  # function used as the parameter for cv.createTrackbar
     pass
 
 
+# creating window for setting Canny thresholds by trackbar
 cv.namedWindow('canny', cv.WINDOW_NORMAL)
-cv.resizeWindow('canny', 640, 360)
-cv.createTrackbar('st', 'canny', 46, 255, pas)
-cv.createTrackbar('en', 'canny', 255, 255, pas)
+cv.resizeWindow('canny', 500, 50)
+cv.createTrackbar('st', 'canny', 46, 255, empty)
+cv.createTrackbar('en', 'canny', 255, 255, empty)
 
+# # creating window for setting hsv values by trackbar
 # cv.namedWindow('hsv', cv.WINDOW_NORMAL)
 # cv.resizeWindow('hsv', 640, 360)
-# cv.createTrackbar('lower_h', 'hsv', 0, 255, pas)
-# cv.createTrackbar('higher_h', 'hsv', 255, 255, pas)
-# cv.createTrackbar('lower_s', 'hsv', 0, 255, pas)
-# cv.createTrackbar('higher_s', 'hsv', 255, 255, pas)
-# cv.createTrackbar('lower_v', 'hsv', 0, 255, pas)
-# cv.createTrackbar('higher_v', 'hsv', 255, 255, pas)
+# cv.createTrackbar('lower_h', 'hsv', 0, 255, empty)
+# cv.createTrackbar('higher_h', 'hsv', 255, 255, empty)
+# cv.createTrackbar('lower_s', 'hsv', 0, 255, empty)
+# cv.createTrackbar('higher_s', 'hsv', 255, 255, empty)
+# cv.createTrackbar('lower_v', 'hsv', 0, 255, empty)
+# cv.createTrackbar('higher_v', 'hsv', 255, 255, empty)
 
+# range of colors to be detected (hsv values)
 colors = {
-    'gold': ((0, 50, 159), (31, 152, 201)),
-    'silver': ((83, 33, 161), (221, 68, 201))
+    'gold': ((45, 50, 80), (50, 100, 100)),
+    'silver': ((0, 0, 75), (255, 30, 90))
 }
 
+# capturing continuous images
 while cap.isOpened():
+    # reading current frame
     ret, image = cap.read()
 
+    # converting to grayscale
     t1, t2 = cv.getTrackbarPos('st', 'canny'), cv.getTrackbarPos('en', 'canny')
     grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    # Gaussian blur to reduce noise
     blur = cv.GaussianBlur(grey, (5, 5), 5)
+
+    # canny edge detection
     canny = cv.Canny(blur, t1, t2)
+
+    # applying some morphological operations
     kernel = np.ones((3, 3), np.uint8)  # kernel for dilation
     img = cv.dilate(canny, kernel, iterations=2)
     img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
 
+    # finding contours
     contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    # print(len(contours))
 
+    # detecting the coins
     coins = []
     for i, contour in enumerate(contours):
+        # approximating the contour shape (circle)
         area = cv.contourArea(contour)
         perimeter = cv.arcLength(contour, closed=True)
-
         epsilon = perimeter * 0.02
         approx = cv.approxPolyDP(contour, epsilon, closed=True)
 
+        # getting dimensions of the contour detected (x-axis, y-axis, width, height)
         x, y, w, h = cv.boundingRect(approx)
 
+        # differentiating contour(coin) by color
         roi = image[y:y + h, x:x + w]
-        if roi is not None and 110000> area > 50000:
+        if roi is not None and area > 5000:  # area set to a min threshold to increase accuracy
+            # marking around the coin
             cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
             cv.circle(image, (x + w // 2, y + h // 2), (w // 2), (0, 255, 0), 5)
+
+            # getting hsv image for checking color
             hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
             for coin, (lower, upper) in colors.items():
-                mask = cv.inRange(hsv_roi,np.array(lower),np.array(upper))
+                mask = cv.inRange(hsv_roi, np.array(lower), np.array(upper))
                 if np.any(mask):
-                    coins.append((coin,area,x,y,w,h))
-            cv.imshow('contour' + str(i), hsv_roi)
+                    # add to coins list if color matched
+                    coins.append((coin, area, x, y, w, h))
+            # cv.imshow('contour' + str(i), hsv_roi)  # to see the detected coins in hsv
 
+    # counting coin values
     total = 0
-    for coin,area,x,y,w,h in coins:
-        if area > 100000:
+    for coin, area, x, y, w, h in coins:
+        # differentiating based on size and displaying the value
+        if area > 70000:
             total += 10
-            cv.putText(image,"10",(x+w//2,y+h//2),cv.FONT_HERSHEY_SIMPLEX,5,(0,255,255),5)
-        elif 80000< area<90000:
+            cv.putText(image, "10", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 10)
+        elif 57000 < area < 59000:
             total += 2
-            cv.putText(image, "2", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 255), 5)
-        elif 60000< area<65000 and coin == 'silver':
-            total += 1
-            cv.putText(image, "1", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 255), 5)
-        elif 66000< area<75000 and coin == 'gold':
+            cv.putText(image, "2", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 10)
+        elif 52000 < area < 56000 and coin == 'gold':
             total += 5
-            cv.putText(image, "5", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 255), 5)
+            cv.putText(image, "5", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 10)
+        elif 50000 > area and coin == 'silver':
+            total += 1
+            cv.putText(image, "1", (x + w // 2, y + h // 2), cv.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 10)
 
+    # displaying total value
+    cv.putText(image, "total = " + str(total), (50, 150), cv.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 8)
 
-    if len(coins)>0:
-        print(coins,len(coins))
-
+    # # to get the hsv values of coins
     # hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     # l_h = cv.getTrackbarPos('lower_h', 'hsv')
     # h_h = cv.getTrackbarPos('higher_h', 'hsv')
@@ -99,12 +122,20 @@ while cap.isOpened():
     # mask = cv.inRange(hsv, lower_hsv, higher_hsv)
     # res = cv.bitwise_and(image, image, mask=mask)
 
-    cv.imshow('out', image)
-    # cv.imshow('canny', img)
-    # cv.imshow('mask',mask)
-    # cv.imshow('res',res)
+    # displaying results
+    cv.imshow('out', image)  # output image
+    # cv.imshow('canny', img)  # canny edge
+    # cv.imshow('mask',mask)  # mask applied
+    # cv.imshow('res',res)  # output after applying mask
+
+    # exit the loop by pressing 'q'
     if cv.waitKey(1) == ord('q'):
         break
 
+# release video capture object
 cap.release()
 cv.destroyAllWindows()
+
+# green = np.uint8([[[172,146,133]]])
+# hsv_green = cv.cvtColor(green,cv.COLOR_BGR2HSV)
+# print( hsv_green )
